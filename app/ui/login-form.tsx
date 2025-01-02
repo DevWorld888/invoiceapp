2112856e7da4c6a8b099f10d3c279ed246128103
Augusto Cs <augustocsuarez1985@gmail.com>
2025-01-02 21:06:08 +1100

    metadata

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -37,6 +37,7 @@ export default function LoginForm() {
                 name="email"
                 placeholder="Enter your email address"
                 required
+                defaultValue="user@nextmail.com" // Default value added
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -57,6 +58,7 @@ export default function LoginForm() {
                 placeholder="Enter password"
                 required
                 minLength={6}
+                defaultValue="123456" // Default value added
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
